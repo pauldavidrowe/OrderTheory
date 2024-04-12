5863fe00bc42b128ba1c45daf2440d9704c37717
pauldavidrowe <pauldavidrowe@gmail.com>
2024-04-12 17:59:26 -0400

    more progress on chapter 2

--- OrderTheory/Chapter02.lean
+++ OrderTheory/Chapter02.lean
@@ -1,4 +1,5 @@
 import OrderTheory.Exercises01
+import Mathlib.Order.Sublattice
 
 open scoped Classical 
 
@@ -17,7 +18,7 @@ open scoped Classical
 -/
 
 /-!
-  # 2.1 Defintions
+  ## 2.1 Defintions
   
   Let `P` be a partial order and let `S ⊆ P` be a subset of `P`. An element 
   `x ∈ P` is an upper bound of `S` if and only if for all `s ∈ S`, `s ≤ x`. A 
@@ -93,7 +94,7 @@ theorem exists_LUB_iff [PartialOrder P] (S : Set P) :
 -/
 
 /-!
-  # 2.2 Top and bottom
+  ## 2.2 Top and bottom
   
   It is easily seen that if `P` has a top element, then `Pᵘ = {⊤}` in which case 
   `sSup P = ⊤`. When `P` has no top element `Pᵘ = ∅` so `sSup P` does not exist. 
@@ -122,7 +123,7 @@ lemma example_2_2d [PartialOrder P] : (∃ x, IsLUB (∅ : Set P) x) ↔ ∃ b :
       exact hb p
     
 /-!
-  # 2.3 Notation
+  ## 2.3 Notation
   
   For Mathlib, we can write `x ⊔ y` for the least upper bound of `x` and `y`
   as long as we have an instance `[SemilatticSup P]`. (We can actually write it
@@ -153,7 +154,7 @@ lemma example_2_2d [PartialOrder P] : (∃ x, IsLUB (∅ : Set P) x) ↔ ∃ b :
 -/
 
 /-!
-  # 2.4 Definitions 
+  ## 2.4 Definitions 
   
   When both `x ⊔ y` and `x ⊓ y` exists for all pairs `x` and `y` in an partial order `P`
   then we call `P` a lattice. This is a type class in Mathlib: `[Lattice P]`. The text doesn't
@@ -172,7 +173,7 @@ lemma example_2_2d [PartialOrder P] : (∃ x, IsLUB (∅ : Set P) x) ↔ ∃ b :
 -/
 
 /-!
-  # 2.5 Remarks on ⊔ and ⊓
+  ## 2.5 Remarks on ⊔ and ⊓
   
   (1) If `(x :P) ≤ y` then `{x, y}ᵘ = ↑ᵖy` and `{x, y}ˡ = ↓ᵖx`. It follows that 
       `x ⊔ y = y` and `x ⊓ y = x`. Setting `x = y` we see that `x ⊔ x = x` and
@@ -242,7 +243,7 @@ lemma example_2_5_5 [Lattice P] {a b c : P} (hba : b ≤ a) (habc : a ≤ b ⊔ 
   exact le_le_iff_eq.mp ⟨h3, h2⟩
   
 /-!
-  # 2.6 Examples 
+  ## 2.6 Examples 
   
   (1) Every chain is a lattice. This is known to Mathlib as LinearOrder.toLattice. 
       This has a bunch of observations about ℝ, ℚ, ℤ, ℕ that follow quite naturally
@@ -417,3 +418,446 @@ local instance instSemilatticeInf {n : Nat} : Lattice (WithTop (WithBot (Fin' n)
         contradiction
   }
     
+/-!
+  ## 2.7 Lattices of subgroups
+  
+  If `G` is a group and ⟨SubG, ⊆⟩ is its ordered set of subgroups, then let 
+  `H`, `K` be subgroups of `G`. It's certainly true that `H ∩ K` is also a
+  subgroup of `G`, so we can define `H ⊓ K` to be the intersection. But 
+  `H ∪ K` is not typically a subgroup. Nevertheless, the subgroup generated
+  by `H ∪ K` *is* a subgroup and is actually `H ⊔ K`. Sadly there is no simple 
+  general formula for this as a set. Normal subgroups are better behaved. For
+  normal subgroups, the set `HK = { h*k | h ∈ H ∧ k ∈ K }` is normal and 
+  serves as the supremum of `H` and `K` in the set of normal subgroups of `G`. 
+  
+  Mathlib does not seem to have any declaration of the type of normal subgroups
+  of a group `G`. After a little effort trying to make one, I realized it's 
+  not trivial. It could be a good exercise, but is not the point of the 
+  current project, so I will defer it until later or until somebody else
+  can do it instead. 
+-/
+
+/-!
+  # Lattices as algebraic structures
+  
+  We begin to look at lattices as algebraic structures where the operators
+  `⊔` and `⊓` are primary. We explore how these are connected with `≤`. 
+-/
+
+/-!
+  ## 2.8 The Connecting Lemma 
+  
+  Let `L` be a lattice and let `a b : L`. Then the following are equivalent:
+  
+  (i) `a ≤ b` 
+  
+  (ii) `a ⊔ b = b` 
+  
+  (iii) `a ⊓ b = a`
+-/
+
+theorem example_2_8_sup_eq_iff_le [Lattice L] {a b : L} : a ⊔ b = b ↔ a ≤ b :=
+  ⟨by rw [sup_eq_right]; tauto, λ le ↦ (example_2_5_1b le).1⟩
+
+theorem example_2_8_inf_eq_iff_le [Lattice L] {a b : L} : a ⊓ b = a ↔ a ≤ b :=
+  ⟨by rw [inf_eq_left]; tauto, λ le ↦ (example_2_5_1b le).2⟩
+
+theorem example2_8_sup_eq_iff_inf_eq [Lattice L] {a b : L} : a ⊔ b = b ↔ a ⊓ b = a := by 
+  rw [example_2_8_inf_eq_iff_le, example_2_8_sup_eq_iff_le] 
+  
+/-!
+  ## 2.9 Theorem
+  
+  Let `L` be a lattice. Then for all `a b c : L`, `⊔` and `⊓` satisfy:
+  
+  (L1) `(a ⊔ b) ⊔ c = a ⊔ (b ⊔ c)` (sup is associative)
+  (L1') `(a ⊓ b) ⊓ c = a ⊓ (b ⊓ c)` (inf is associative)
+  (L2) `a ⊔ b = b ⊔ a` (sup is commutative)
+  (L2') `a ⊓ b = b ⊓ a` (inf is commutative)
+  (L3) `a ⊔ a = a` (sup is idempotent)
+  (L3') `a ⊓ a = a` (inf is idempotent)
+  (L4) `a ⊔ (a ⊓ b) = a` (absorption for inf then sup)
+  (L4') `a ⊓ (a ⊔ b) = a` (absorption for sup then inf)
+-/
+
+/-- This is known in Mathlib as `sup_assoc`. The Mathlib proof is recreated here. -/
+theorem example_2_9_sup_assoc [Lattice L] {a b c : L} : (a ⊔ b) ⊔ c = a ⊔ (b ⊔ c) := 
+  eq_of_forall_ge_iff (λ x ↦ by simp only [sup_le_iff]; rw [and_assoc])
+  
+/-- This is known in Mathlib as `inf_assoc`. This is provable as the dual of the above. -/
+theorem example_2_9_inf_assoc [Lattice L] {a b c : L} : (a ⊓ b) ⊓ c = a ⊓ (b ⊓ c) :=
+  @example_2_9_sup_assoc Lᵒᵈ _ _ _ _ 
+  
+/-- This is known in Mathlib as `sup_comm`. -/
+theorem example_2_9_sup_comm [Lattice L] {a b : L} : a ⊔ b = b ⊔ a := 
+  eq_of_forall_ge_iff (λ x ↦ by simp only [sup_le_iff]; rw [and_comm])
+  
+/-- This is known in Mathlib as `inf_comm`. -/
+theorem example_2_9_inf_comm [Lattice L] {a b : L} : a ⊓ b = b ⊓ a := 
+  eq_of_forall_le_iff (λ x ↦ by simp only [le_inf_iff]; rw [and_comm])
+  
+/-- This is known in Mathlib as `sup_idem` and proved by simp. -/
+theorem example_2_9_sup_idem [Lattice L] {a : L} : a ⊔ a = a := by simp 
+
+/-- This is known in Mathlib as `inf_idem` and proved by simp. -/
+theorem example_2_9_inf_idem [Lattice L] {a : L} : a ⊓ a = a := by simp 
+
+/-- This is known in Mathlib as `sup_inf_self`. -/
+theorem example_2_9_sup_inf_self [Lattice L] {a b : L} : a ⊔ (a ⊓ b) = a := by 
+  rw [sup_comm, example_2_8_sup_eq_iff_le]
+  exact inf_le_left 
+
+/-- This is known in Mathlib as `inf_sup_self`. -/
+theorem example_2_9_inf_sup_self [Lattice L] {a b : L} : a ⊓ (a ⊔ b) = a := 
+  @example_2_9_sup_inf_self Lᵒᵈ _ _ _ 
+  
+/-!
+  Let `L` be a set (type) equipped with two operations `⊔` and `⊓` 
+  that satisfy (L1)-(L4) and (L1')-(L4') above. Then 
+  
+  (i) For all `a b : L`, we have `a ⊔ b = b` if and only if `a ⊓ b = a`
+  
+  (ii) Define `≤` on `L` by `a ≤ b` if and only if `a ⊔ b = b`. Then `L` is a 
+  partial order under `≤`. 
+  
+  (iii) With `≤` as above, `L` is actually a lattice in which `⊔` is sup and
+  `⊓` is inf.
+  
+  This is essentially `Lattice.mk'` which does not even use `L3` and `L3'`. I 
+  will try to get by without either of them. 
+-/
+
+theorem example_2_10_i [Sup L] [Inf L] 
+    (L2 : ∀ {a b : L}, a ⊔ b = b ⊔ a)
+    (L2': ∀ {a b : L}, a ⊓ b = b ⊓ a)
+    (L4 : ∀ {a b : L}, a ⊔ (a ⊓ b) = a)
+    (L4': ∀ {a b : L}, a ⊓ (a ⊔ b) = a) {a b : L} :
+    a ⊔ b = b ↔ a ⊓ b = a := by
+  constructor <;> intro h 
+  · rw [←h]; exact L4' 
+  · rw [←h, L2, L2']; exact L4  
+
+def LatPO [Sup L] [Inf L] 
+    (L1 : ∀ {a b c : L}, (a ⊔ b) ⊔ c = a ⊔ (b ⊔ c))
+    (L1': ∀ {a b c : L}, (a ⊓ b) ⊓ c = a ⊓ (b ⊓ c))
+    (L2 : ∀ {a b : L}, a ⊔ b = b ⊔ a)
+    (L2': ∀ {a b : L}, a ⊓ b = b ⊓ a)
+    (L4 : ∀ {a b : L}, a ⊔ (a ⊓ b) = a)
+    (L4': ∀ {a b : L}, a ⊓ (a ⊔ b) = a) : Lattice L := 
+  have L3 : ∀ (a : L), a ⊔ a = a := λ a ↦
+    calc
+      a ⊔ a = a ⊔ a ⊓ (a ⊔ a) := by rw [L4']
+      _ = a := by rw [L4]
+  { 
+    le := λ a b ↦ a ⊔ b = b 
+    le_refl := L3
+    le_trans := by 
+      intro a b c le1 le2
+      simp at le1 le2 ⊢
+      rw [←le2, ←L1, le1] 
+    le_antisymm := by
+      intro a b le1 le2
+      simp at le1 le2 
+      rw [←le1]; nth_rewrite 1 [←le2]; rw [L2]
+    le_sup_left := by 
+      intro a b; simp
+      rw [←L1, L3]
+    le_sup_right := by 
+      intro a b; simp
+      rw [L2, L1, L3]
+    sup_le := by 
+      intro a b c le1 le2
+      simp at le1 le2 ⊢
+      rw [L1, le2, le1]
+    inf_le_left := by 
+      intro a b; simp
+      rw [L2, L4]
+    inf_le_right := by 
+      intro a b; simp
+      rw [L2, L2', L4]
+    le_inf := by
+      intro a b c le1 le2
+      simp at le1 le2 ⊢
+      rw [example_2_10_i L2 L2' L4 L4'] at le1 le2 ⊢
+      rw [←L1', le1, le2]  
+  }
+
+/-!
+  ## 2.11 Stocktaking 
+  
+  So Lattices can be completely characterized by `⊔` and `⊓`. In all Lattices,
+  the sup and inf are related to `≤` exactly as above. So, we can take the 
+  algebraic properties as simply given without having to think about LUBs and GLBs
+  if that happens to be a more convenient way of proceeding. 
+  
+  Observe that associativity of `⊔` means that `sSup {a₁, ..., aₙ} = a₁ ⊔ ... ⊔ aₙ` (by
+  a simple inductive argument). As a consequence, suprema exist for any finite set of 
+  elements of a lattice. The inductive argument only applies to finite sets, and of
+  course the suprema of infinite subsets of a lattice need not exist in general. So, 
+  in general, there need not be an instance of `SupSet L` if we have `Lattice L`. 
+-/
+
+/-!
+  ## 2.12 Definitions
+  
+  Let `L` be a lattice. It could happen that it has both a `⊤` and `⊥`. We want to 
+  explore how to think of these from the algebraic point of view instead of the 
+  order-theoretic point of view. 
+  
+  `L` has a "one" if there is some element `1 : L` such that for all `a : L`, 
+  `a ⊓ 1 = a`. Similarly, `L` has a "zero" if there is some element `0 : L` 
+  such that `a ⊔ 0 = a` for all `a : L`. Then it follows that `L` has an 
+  algebraic `1` if and only if `L` has an order-theoretic `⊤`, and dually for 
+  `0` and `⊥`. 
+  
+  A lattice with a `0` and `1` is called bounded. There does not seem to be a single class for 
+  bounded lattices in Mathlib. Rather, there is `BoundedOrder L` which is defined
+  to be the conjunction of `OrderTop L` and `OrderBot L`. So, to say that `L` is
+  a bounded lattice we would have to write `[BoundedOrder L] [Lattice L]`. 
+  
+  Note that all finite lattices are automatically bounded with `sSup L = 1` and 
+  `sInf L = 0`. In fact, Mathlib does not seem to have an instance of `SupSet` for
+  finite types with suprema. Can I just make one here? In the spirit of not 
+  worrying about finite sets in this pass, I will leave it alone. 
+  
+  Recalling 2.6(5), note that `⟨ℕ, lcm, gcd⟩` is bounded with `1 = 0` and `0 = 1`.
+-/
+
+section 
+
+def LNat : Type := ℕ
+
+@[simp]
+instance LNat.instSuplocal : Sup LNat := { sup := Nat.lcm }
+
+@[simp]
+instance LNat.instInflocal : Inf LNat := { inf := Nat.gcd }
+
+instance LNat.instCCMWZ : CancelCommMonoidWithZero LNat := Nat.cancelCommMonoidWithZero
+instance LNat.instNGCDM : NormalizedGCDMonoid LNat := by
+  haveI I : NormalizedGCDMonoid Nat := by infer_instance
+  exact I 
+
+lemma gcd_lcm_self (n m : LNat) : Nat.gcd n (Nat.lcm n m) = n := by 
+  exact Nat.gcd_eq_left (Nat.dvd_lcm_left n m)
+
+lemma lcm_gcd_self (n m : LNat) : Nat.lcm n (Nat.gcd n m) = n  := by 
+  have h := Nat.gcd_dvd_left n m
+  exact (lcm_eq_left_iff n _ (by simp)).mpr h
+
+instance example_2_12_L : Lattice LNat := 
+  Lattice.mk' lcm_comm lcm_assoc gcd_comm gcd_assoc lcm_gcd_self gcd_lcm_self
+
+def example_2_12_BO : BoundedOrder LNat := 
+  {
+    top := 0
+    bot := 1
+    le_top := λ a ↦ Nat.lcm_zero_right a 
+    bot_le := λ a ↦ by
+      change 1 ⊔ a = a 
+      rw [example_2_10_i (by simp [Nat.lcm_comm]) (by simp [Nat.gcd_comm]) (by simp [lcm_gcd_self]) (by simp [gcd_lcm_self])]
+      exact Nat.gcd_one_left a 
+  }
+
+end section
+
+/-!
+  # Sublattices, products and homomorphisms 
+  
+  We now show how to derive more lattices from existing lattices
+-/
+
+/-!
+  ## 2.13 Sulattices
+  
+  Let `L` be a lattice and `∅ ≠ M ⊆ L`. Then `M` is a sublattice of `L`
+  if and only if
+  
+  `a,b ∈ M` implies `a ⊔ b ∈ M` and `a ⊓ b ∈ M`. 
+  
+  This manifests in Mathlib as `Sublattice (M : Set L)` that has the 
+  `carrier` set of elements of `M`, `supClosed'`, a proof that sups
+  of elements of `M` are also in `M`, and its dual `infClosed'`. 
+-/
+
+/-!
+  ## 2.14 Examples
+  
+  (1) Any one-element subset of a lattice is a sublattice. 
+-/
+
+def example_2_14a [Lattice α] (x : α) : Sublattice α := 
+  {
+    carrier := {x}
+    supClosed' := by 
+      intro a mema b memb
+      simp at mema memb 
+      subst a; subst b 
+      simp
+    infClosed' := by 
+      intro a mema b memb
+      simp at mema memb 
+      subst a; subst b 
+      simp 
+  }
+  
+/-!
+  More generally, any non-empty chain in a lattice is a sublattice.
+-/
+  
+def example_2_14b [Lattice α] {L : Set α} (h : IsChain_le L) : 
+    Sublattice α := 
+  {
+    carrier := L
+    supClosed' := by 
+      intro a mema b memb        
+      by_cases eq : a = b 
+      · subst a; simp; exact memb 
+      · specialize h mema memb eq 
+        cases h with 
+        | inl h => simp_all
+        | inr h => simp_all
+    infClosed' := by 
+      intro a mema b memb
+      by_cases eq : a = b 
+      · subst a; simp; exact memb 
+      · specialize h mema memb eq 
+        cases h with 
+        | inl h => simp_all
+        | inr h => simp_all  
+  }
+  
+/-!
+  (2) More diagram-based stuff 
+  
+  (3) A subset `M` of a lattice `L` may be a lattice in its own right
+  without being a sublattice of `L`. This is shown in one of the diagrams.
+-/
+
+/-!
+  ## 2.15 Products 
+  
+  Let `L` and `K` be lattices. Define `⊔` and `⊓` coordinate-wise on 
+  `L × K`, as follows:
+    
+    (l₁, k₁) ⊔ (l₂, k₂) = (l₁ ⊔ l₂, k₁ ⊔ k₂)
+    
+    (l₁, k₁) ⊓ (l₂, k₂) = (l₁ ⊓ l₂, k₁ ⊓ k₂)
+    
+  One can check that the algebraic properties of `⊔` and `⊓` are satisfied
+  by these definitions, so the product also forms a lattice. This 
+  is the default lattice instance on `L × K` in Mathlib assuming `L` and `K`
+  are both lattices. 
+  
+  It is routine to show that `L × K` always contains sublattices isomorphic
+  to `L` and to `K` (assuming they're nonempty), by taking `{l₀} × K` and
+  `L × {k₀}` for any `l₀` and `k₀`. I'll only do one of these. 
+-/
+
+lemma exercise_2_15_a [Lattice L] [Lattice K] (l₀  : L) : 
+    K ≃o ({ (l, _) | l = l₀ } : Set (L × K)) := 
+  {
+    toFun := λ k ↦ ⟨(l₀, k), by simp⟩
+    invFun := λ lk ↦ lk.val.2
+    left_inv := by intro k; simp
+    right_inv := by 
+      intro ⟨(l, k), prf⟩; simp at prf ⊢
+      symm; exact prf 
+    map_rel_iff' := by 
+      intro k1 k2
+      constructor <;> intro le
+      · exact le.2
+      · exact ⟨by rfl, le⟩ 
+  }
+
+/-!
+  Lattice homomorphisms are maps that preserve sups and infs. It will be 
+  important to understand how these maps relate to order preserving maps
+  on the lattices viewed simply as partial orders. But first we have 
+  to get lattice homomorphisms defined. 
+  
+  In Mathlib, a `SupHom α β` requires both `α` and `β` to have `⊔` defined.
+  It then consists of a function `f : α → β` and a proof that it preserves sups,
+  namely `f (a₁ ⊔ a₂) = f a₁ ⊔ f a₂`. Then a lattice homomorphism from 
+  `α` to `β` is a SupHom that also preserves inf. Thus `LatticeHom α β`
+  comes equipped with a function, and two proofs that it preserves sups and
+  infs. Those proofs are called `map_sup'` and `map_inf'`. 
+  
+  The text defines a Lattice Isomorphism as a bijective Lattice Homomorphism.
+  There is no `LatticeIso α β` defined in Mathlib, so it will probably turn
+  out that an `OrderIso α β` between lattices `α` and `β` must be a bijective
+  lattice homomorphism, but we'll see! Similarly, the text defines a 
+  lattice embedding to be a lattice hom that is injective. No such definition
+  exists in Mathlib, so we'll have to see why. 
+-/
+
+/-!
+  ## 2.17 Remarks
+  
+  (1) It is easy to check that the inverse of a lattice isomorphism is a
+  lattice hom, and hence also a lattice isomorphism. 
+-/
+
+lemma example_2_17_1 [Lattice L] [Lattice K] (η : LatticeHom L K) (invFun : K → L)
+    (leftInv : Function.LeftInverse invFun η.toFun) 
+    (rightInv : Function.RightInverse invFun η.toFun) 
+    : LatticeHom K L := 
+  have h : Function.Bijective η.toFun := by 
+    apply Function.bijective_iff_has_inverse.mpr
+    use invFun 
+  {
+    toFun := invFun 
+    map_sup' := by 
+      intro a b 
+      obtain ⟨al, hal⟩ := h.2 a 
+      obtain ⟨bl, hbl⟩ := h.2 b
+      subst a; subst b 
+      rw [←η.map_sup']
+      rw [leftInv, leftInv, leftInv]
+    map_inf' := by 
+      intro a b 
+      simp
+      obtain ⟨al, hal⟩ := h.2 a 
+      obtain ⟨bl, hbl⟩ := h.2 b
+      subst a; subst b 
+      rw [←η.map_inf']
+      rw [leftInv, leftInv, leftInv]
+  }
+  
+/-!
+  (2) The text introduces notation for when `K` has a sublattice that is isomorphic
+  to `L`. We will see below that when this happens, then there will be an 
+  OrderEmbedding. 
+  
+  (3) Lattice homs need not preserve `⊤` and `⊥` if they exist, apparently. 
+  So it's natural to define a `BoundedLatticeHom` that is a `LatticeHom` that
+  additionally preserves ⊤ and ⊥. The text calls these `{0, 1}-homomorphisms`. 
+  These apparently don't show up until chapters 5 and 11. 
+-/
+
+/-!
+  ## 2.18 Examples 
+  
+  The text returns to various previous examples. The key takeaway is that
+  not every `OrderHom` between lattices is also a `LatticeHom`. However, we
+  will see below, that every `OrderIso` is a Lattice Isomorphism. This explains
+  why we don't need a separate class for Lattice Isomorphisms. 
+-/
+
+/-!
+  ## 2.19 Proposition 
+  
+  Let `L` and `K` be lattices and `f : L → K` a map. 
+  
+  (i) The following are equivalent: 
+  
+  (a) `f` is order-preserving 
+  
+  (b) ∀ a b : L, f a ⊔ f b ≤ f (a ⊔ b)
+  
+  (c) ∀ a b : L, f (a ⊓ b) ≤ f a ⊓ f b
+  
+  In particular, if `f` is a lattice hom, it is order preserving. 
+  
+  (ii) `f` is a lattice isomorphism if and only if it is an order isomorphism.
+-/
